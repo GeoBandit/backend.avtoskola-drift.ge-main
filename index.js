cb7dec727a21bb6f5fce6830b6dbe1f2b7b4efeb
GeoBandit <35873181+GeoBandit@users.noreply.github.com>
2025-07-25 12:20:33 +0400

    add link to app.use

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(
   "https://new-traffic-frontend.vercel.app",
   "http://localhost:3000",
   "https://avtoskola-varketilshi-six.vercel.app"
+  "https://fronend-avtoskola-drift-ge-main.vercel.app"
 ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
